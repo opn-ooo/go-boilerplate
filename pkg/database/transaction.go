00c1433eb8e7dfda5b5f32ecd03471ddacbb5a74
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2021-03-01 12:02:32 +0800

    Update transaction

--- pkg/database/transaction.go
+++ pkg/database/transaction.go
@@ -2,32 +2,34 @@ package database
 
 import (
 	"context"
-
 	"gorm.io/gorm"
 )
 
 type TransactionInterface interface {
-	WithInTransaction(ctx context.Context, fn func(ctx context.Context) error) error
+	WithInTransaction(ctx *context.Context, fn func(ctx context.Context) error) error
 }
 
-type transactionManager struct {
+type Transaction struct {
 	db *gorm.DB
 }
 
 func NewTransaction(db *gorm.DB) TransactionInterface {
-	return &transactionManager{db}
+	return &Transaction{db}
 }
 
 var transactionKey = struct{}{}
 
-func (r *transactionManager) WithInTransaction(ctx context.Context, fn func(ctx context.Context) error) error {
-	return r.db.Transaction(func(tx *gorm.DB) error {
-		ctx = context.WithValue(ctx, &transactionKey, tx)
-		return fn(ctx)
+func (r *Transaction) WithInTransaction(ctx *context.Context, fn func(ctx context.Context) error) error {
+	return r.db.Transaction(func(transaction *gorm.DB) error {
+		*ctx = context.WithValue(*ctx, &transactionKey, transaction)
+		return fn(*ctx)
 	})
 }
 
-func GetTransaction(context context.Context) (*gorm.DB, bool) {
-	transaction, ok := context.Value(&transactionKey).(*gorm.DB)
+func GetTransaction(context *context.Context) (*gorm.DB, bool) {
+	if context == nil {
+		return nil, false
+	}
+	transaction, ok := (*context).Value(&transactionKey).(*gorm.DB)
 	return transaction, ok
 }
